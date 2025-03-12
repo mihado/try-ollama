f1b138d4c7f7e8abe3667b9c5455aa5df283c389
Minh Ha <mihado@minh.im>
2025-03-12 22:22:50 +0000

    chore: upgrade-ollama.sh

---
+++ upgrade-ollama.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -rf tmp/ollama
+mkdir -p tmp/ollama
+curl -fsSL https://ollama.com/download/ollama-linux-amd64.tgz | tar zx -C tmp/ollama
+
+ollama --version
+
+if [ "$(ollama --version)" == "$(tmp/ollama/bin/ollama --version)" ]; then
+	echo "ollama is up to date"
+	exit 0
+else
+	echo "upgrading ollama"
+fi
+
+rm -f /usr/bin/ollama
+rm -rf /usr/lib/ollama
+mkdir /usr/lib/ollama
+cp -f tmp/ollama/bin/ollama /usr/bin
+cp -r tmp/ollama/lib/ollama /usr/lib/
+
+sudo systemctl stop ollama
+sudo chmod +x /usr/bin/ollama
+sudo systemctl restart ollama
+
+# check if ollama is up to date & running
+ollama --version
